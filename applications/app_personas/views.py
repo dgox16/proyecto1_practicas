1e6baabb00a43324c5df8148c71628df520bb6b9
Diego <dgox16@gmail.com>
2023-02-05 21:18:04 -0600

    Se agrego la funcion de editar las formularios

--- applications/app_personas/views.py
+++ applications/app_personas/views.py
@@ -1,11 +1,13 @@
 from django.contrib.auth.mixins import LoginRequiredMixin
-from django.shortcuts import HttpResponseRedirect
+from django.shortcuts import HttpResponseRedirect, get_object_or_404
 from django.urls import reverse, reverse_lazy
 from django.views.generic import CreateView, DeleteView, ListView, UpdateView
 from django_addanother.views import CreatePopupMixin
 
 from applications.app_PLD.forms import FormPld
+from applications.app_PLD.models import Pld
 from applications.app_trabajo.forms import FormSocio, FormTrabajo
+from applications.app_trabajo.models import Socio, Trabajo
 
 from .forms import FormPersona
 from .models import Clasificacion, Formulario, Persona, Sucursal, Tipo
@@ -39,7 +41,7 @@ class VistaAgregarTodo(LoginRequiredMixin, CreatePopupMixin, CreateView):
                 persona=persona, pld=pld, socio=socio, trabajo=trabajo
             )
             formulario.save()
-            return HttpResponseRedirect(reverse("personas_app:todas_personas"))
+            return HttpResponseRedirect(reverse("personas_app:todos_formularios"))
         else:
             return self.form_invalid(**kwargs)
 
@@ -137,35 +139,42 @@ class VistaModificarFormulario(LoginRequiredMixin, UpdateView):
     form_class_trabajo = FormTrabajo
 
     def post(self, request, *args, **kwargs):
-        persona_form = self.form_class(request.POST)
-        pld_form = self.form_class_pld(request.POST)
-        socio_form = self.form_class_socio(request.POST)
-        trabajo_form = self.form_class_trabajo(request.POST)
+        idPersona = self.get_object().persona.id
+        obj_persona = get_object_or_404(Persona, id=idPersona)
+        persona_form = self.form_class(request.POST, instance=obj_persona)
+        idPld = self.get_object().pld.id
+        obj_pld = get_object_or_404(Pld, id=idPld)
+        pld_form = self.form_class_pld(request.POST, instance=obj_pld)
+        idSocio = self.get_object().socio.id
+        obj_socio = get_object_or_404(Socio, id=idSocio)
+        socio_form = self.form_class_socio(request.POST, instance=obj_socio)
+        idTrabajo = self.get_object().trabajo.id
+        obj_trabajo = get_object_or_404(Trabajo, id=idTrabajo)
+        trabajo_form = self.form_class_trabajo(request.POST, instance=obj_trabajo)
 
         persona = persona_form.save(commit=False)
-        if persona_form.is_valid() and pld_form.is_valid() and socio_form.is_valid():
+        if persona_form.is_valid() and pld_form.is_valid():
             pld = pld_form.save(commit=False)
             socio = socio_form.save(commit=False)
             trabajo = trabajo_form.save(commit=False)
+            print(persona)
             persona.save()
             pld.save()
             socio.save()
             trabajo.save()
-            formulario = Formulario(
-                persona=persona, pld=pld, socio=socio, trabajo=trabajo
-            )
-            formulario.save()
-            return HttpResponseRedirect(reverse("personas_app:todas_personas"))
+            return HttpResponseRedirect(reverse("personas_app:todos_formularios"))
         else:
-            return self.form_invalid(**kwargs)
+            return HttpResponseRedirect(reverse("personas_app:todas_personas"))
 
     def get_context_data(self, **kwargs):
-        formulario = self.get_object().persona.id
-        form2 = Persona.objects.get(id=formulario)
-        form = FormPersona(instance=form2)
-        formPLD = FormPld()
-        formSocio = FormSocio()
-        formTrabajo = FormTrabajo()
+        idPersona = self.get_object().persona.id
+        idPld = self.get_object().pld.id
+        idSocio = self.get_object().socio.id
+        idTrabajo = self.get_object().trabajo.id
+        form = FormPersona(instance=Persona.objects.get(id=idPersona))
+        formPLD = FormPld(instance=Pld.objects.get(id=idPld))
+        formSocio = FormSocio(instance=Socio.objects.get(id=idSocio))
+        formTrabajo = FormTrabajo(instance=Trabajo.objects.get(id=idTrabajo))
         context = super(VistaModificarFormulario, self).get_context_data(**kwargs)
         context["jquery"] = "admin/js/vendor/jquery/jquery.js"
         context["form"] = form
