a1ef24051b6264ecbe3d83c67ed42d4f98170911
Diego <dgox16@gmail.com>
2023-02-10 21:44:50 -0600

    Se agrego la opcion de tipo en links. Falta diseño para una mejor visualizacion

--- applications/app_pages/models.py
+++ applications/app_pages/models.py
@@ -2,11 +2,17 @@ from django.db import models
 
 
 class BarraLinks(models.Model):
+    TIPO = (
+        (1, "Principal"),
+        (2, "Catalogo vistas"),
+    )
+
     titulo = models.CharField("Titulo:", max_length=50)
     url = models.CharField("URL:", max_length=50)
     orden = models.CharField("Orden:", max_length=50)
     icono = models.CharField("Icono:", max_length=50, default="face")
     is_active = models.BooleanField("Esta activo")
+    tipo = models.IntegerField(choices=TIPO, default=1)
 
     class Meta:
         ordering = ["orden"]
--- applications/app_pages/views.py
+++ applications/app_pages/views.py
@@ -6,5 +6,6 @@ from applications.app_pages.models import BarraLinks
 class BarraLateral(ContextMixin):
     def get_context_data(self, *args, **kwargs):
         context = super().get_context_data(*args, **kwargs)
-        context["nav_links"] = BarraLinks.objects.filter(is_active=True)
+        context["links_principales"] = BarraLinks.objects.filter(tipo=1)
+        context["links_catalogo_vistas"] = BarraLinks.objects.filter(tipo=2)
         return context
