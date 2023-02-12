c14dcd19a17d7d5e45d3df074d0c0465b4869935
Diego <dgox16@gmail.com>
2023-02-11 23:30:58 -0600

    Se mejoro el diseño de dropdowns y se agregaron más categorias

--- applications/app_pages/models.py
+++ applications/app_pages/models.py
@@ -3,14 +3,17 @@ from django.db import models
 
 class BarraLinks(models.Model):
     TIPO = (
-        (1, "Principal"),
-        (2, "Catalogo vistas"),
+        (1, "Acciones"),
+        (2, "Catalogo trabajo"),
+        (3, "Catalogo pld"),
+        (4, "Catalogo personas"),
+        (5, "Catalogo formularios"),
+        (6, "Catalogo domicilo"),
     )
 
     titulo = models.CharField("Titulo:", max_length=50)
     url = models.CharField("URL:", max_length=50)
     orden = models.CharField("Orden:", max_length=50)
-    icono = models.CharField("Icono:", max_length=50, default="face")
     is_active = models.BooleanField("Esta activo")
     tipo = models.IntegerField(choices=TIPO, default=1)
 
--- applications/app_pages/views.py
+++ applications/app_pages/views.py
@@ -6,6 +6,10 @@ from applications.app_pages.models import BarraLinks
 class BarraLateral(ContextMixin):
     def get_context_data(self, *args, **kwargs):
         context = super().get_context_data(*args, **kwargs)
-        context["links_principales"] = BarraLinks.objects.filter(tipo=1)
-        context["links_catalogo_vistas"] = BarraLinks.objects.filter(tipo=2)
+        context["links_acciones"] = BarraLinks.objects.filter(tipo=1)
+        context["links_catalogo_trabajos"] = BarraLinks.objects.filter(tipo=2)
+        context["links_catalogo_pld"] = BarraLinks.objects.filter(tipo=3)
+        context["links_catalogo_persona"] = BarraLinks.objects.filter(tipo=4)
+        context["links_catalogo_formulario"] = BarraLinks.objects.filter(tipo=5)
+        context["links_catalogo_domicilio"] = BarraLinks.objects.filter(tipo=6)
         return context
