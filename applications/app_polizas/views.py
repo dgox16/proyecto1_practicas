ecdabfc36c6da91db0db80edc25a48cee92b2729
Diego <dgox16@gmail.com>
2023-03-13 21:03:20 -0600

    Se optimizo el codigo para mejorar la escabilidad. Se pueden añadir los detalles necesarios solo ajustando un valor

--- applications/app_polizas/views.py
+++ applications/app_polizas/views.py
@@ -25,48 +25,31 @@ class VistaAgregarTodo(BarraLateral, LoginRequiredMixin, CreatePopupMixin, Creat
     form_class_detalle_poliza = FormDetallePoliza
     form_class_poliza_egreso = FormPolizaEgreso
 
+    global numDetalles
+    numDetalles = 50
+
     def post(self, request, *args, **kwargs):
         poliza_form = self.form_class(request.POST)
-        detalleP_form = self.form_class_detalle_poliza(request.POST, prefix="_0")
-        detalleP1_form = self.form_class_detalle_poliza(request.POST, prefix="_1")
-        detalleP2_form = self.form_class_detalle_poliza(request.POST, prefix="_2")
-        detalleP3_form = self.form_class_detalle_poliza(request.POST, prefix="_3")
-        detalleP4_form = self.form_class_detalle_poliza(request.POST, prefix="_4")
-        detalleP5_form = self.form_class_detalle_poliza(request.POST, prefix="_5")
         polizaE_form = self.form_class_poliza_egreso(request.POST)
-
         poliza = poliza_form.save(commit=False)
+
         poliza.numero = 1
         poliza.usuarioElabora = request.user
-        if poliza_form.is_valid() and detalleP_form.is_valid():
-            detallePoliza = detalleP_form.save(commit=False)
-            detallePoliza.poliza = poliza
+        if poliza_form.is_valid():
             poliza.save()
-            detallePoliza.save()
             if polizaE_form.is_valid():
                 polizaEgreso = polizaE_form.save(commit=False)
                 polizaEgreso.poliza = poliza
                 polizaEgreso.save()
-            if detalleP1_form.is_valid():
-                detallePoliza1 = detalleP1_form.save(commit=False)
-                detallePoliza1.poliza = poliza
-                detallePoliza1.save()
-            if detalleP2_form.is_valid():
-                detallePoliza2 = detalleP2_form.save(commit=False)
-                detallePoliza2.poliza = poliza
-                detallePoliza2.save()
-            if detalleP3_form.is_valid():
-                detallePoliza3 = detalleP3_form.save(commit=False)
-                detallePoliza3.poliza = poliza
-                detallePoliza3.save()
-            if detalleP4_form.is_valid():
-                detallePoliza4 = detalleP4_form.save(commit=False)
-                detallePoliza4.poliza = poliza
-                detallePoliza4.save()
-            if detalleP5_form.is_valid():
-                detallePoliza5 = detalleP5_form.save(commit=False)
-                detallePoliza5.poliza = poliza
-                detallePoliza5.save()
+
+            for n in range(numDetalles):
+                detalleForm = self.form_class_detalle_poliza(
+                    request.POST, prefix="_" + str(n + 1)
+                )
+                if detalleForm.is_valid():
+                    detallePoliza = detalleForm.save(commit=False)
+                    detallePoliza.poliza = poliza
+                    detallePoliza.save()
 
             return HttpResponseRedirect(reverse("polizas_app:todas_polizas"))
         else:
@@ -74,22 +57,14 @@ class VistaAgregarTodo(BarraLateral, LoginRequiredMixin, CreatePopupMixin, Creat
 
     def get_context_data(self, **kwargs):
         form = FormPoliza()
-        formPoliza = FormDetallePoliza(prefix="_0")
-        formPoliza1 = FormDetallePoliza(prefix="_1")
-        formPoliza2 = FormDetallePoliza(prefix="_2")
-        formPoliza3 = FormDetallePoliza(prefix="_3")
-        formPoliza4 = FormDetallePoliza(prefix="_4")
-        formPoliza5 = FormDetallePoliza(prefix="_5")
+        formsDetalle = []
+        for n in range(numDetalles):
+            formsDetalle.append(FormDetallePoliza(prefix="_" + str(n + 1)))
         formPolizaEgreso = FormPolizaEgreso()
         context = super(VistaAgregarTodo, self).get_context_data(**kwargs)
         context["jquery"] = "admin/js/vendor/jquery/jquery.js"
         context["formPoliza"] = form
-        context["formDetallePoliza"] = formPoliza
-        context["formDetallePoliza1"] = formPoliza1
-        context["formDetallePoliza2"] = formPoliza2
-        context["formDetallePoliza3"] = formPoliza3
-        context["formDetallePoliza4"] = formPoliza4
-        context["formDetallePoliza5"] = formPoliza5
+        context["forms"] = formsDetalle
         context["formPolizaEgreso"] = formPolizaEgreso
         return context
 
--- applications/static/scripts/polizas.js
+++ applications/static/scripts/polizas.js
@@ -1,138 +1,79 @@
 const el = document.getElementById("id_tipo");
 const box = document.getElementById("poliza_egreso");
 el.addEventListener("change", function handleChange(event) {
-	if (event.target.value === "E") {
-		box.style.display = "block";
-		document.getElementById("id_beneficiario").required = true;
-		document.getElementById("id_banco").required = true;
-		document.getElementById("id_cheque").required = true;
-	} else {
-		box.style.display = "none";
-		document.getElementById("id_beneficiario").required = false;
-		document.getElementById("id_banco").required = false;
-		document.getElementById("id_cheque").required = false;
-	}
+    if (event.target.value === "E") {
+        box.style.display = "block";
+        document.getElementById("id_beneficiario").required = true;
+        document.getElementById("id_banco").required = true;
+        document.getElementById("id_cheque").required = true;
+    } else {
+        box.style.display = "none";
+        document.getElementById("id_beneficiario").required = false;
+        document.getElementById("id_banco").required = false;
+        document.getElementById("id_cheque").required = false;
+    }
 });
 
-function colocarValores(numero, estado) {
-	if (estado) {
-		if (numero == 1) {
-			tab1.style.display = "block";
-		}
-		if (numero == 2) {
-			tab2.style.display = "block";
-		}
-		if (numero == 3) {
-			tab3.style.display = "block";
-		}
-		if (numero == 4) {
-			tab4.style.display = "block";
-		}
-		if (numero == 5) {
-			tab5.style.display = "block";
-		}
-		document.getElementById("id__" + numero + "-cuenta").required = true;
-		document.getElementById("id__" + numero + "-cargo").required = true;
-		document.getElementById("id__" + numero + "-sucursal").required = true;
-		document.getElementById("id__" + numero + "-abono").required = true;
-		document.getElementById("id__" + numero + "-proveedor").required = true;
-		document.getElementById("id__" + numero + "-concepto").required = true;
-		document.getElementById("id__" + numero + "-iva").required = true;
-	} else {
-		if (numero == 1) {
-			tab1.style.display = "none";
-		}
-		if (numero == 2) {
-			tab2.style.display = "none";
-		}
-		if (numero == 3) {
-			tab3.style.display = "none";
-		}
-		if (numero == 4) {
-			tab4.style.display = "none";
-		}
-		if (numero == 5) {
-			tab5.style.display = "none";
-		}
-		document.getElementById("id__" + numero + "-cuenta").required = false;
-		document.getElementById("id__" + numero + "-cargo").required = false;
-		document.getElementById("id__" + numero + "-sucursal").required = false;
-		document.getElementById("id__" + numero + "-abono").required = false;
-		document.getElementById("id__" + numero + "-proveedor").required = false;
-		document.getElementById("id__" + numero + "-concepto").required = false;
-		document.getElementById("id__" + numero + "-iva").required = false;
-	}
-}
+const colocarValores = (numero, estado) => {
+    if (estado) {
+        document.getElementById("poliza_detalle" + numero).style.display =
+            "block";
+        document.getElementById("id__" + numero + "-cuenta").required = true;
+        document.getElementById("id__" + numero + "-cargo").required = true;
+        document.getElementById("id__" + numero + "-sucursal").required = true;
+        document.getElementById("id__" + numero + "-abono").required = true;
+        document.getElementById("id__" + numero + "-proveedor").required = true;
+        document.getElementById("id__" + numero + "-concepto").required = true;
+        document.getElementById("id__" + numero + "-iva").required = true;
+    } else {
+        document.getElementById("poliza_detalle" + numero).style.display =
+            "none";
+        document.getElementById("id__" + numero + "-cuenta").required = false;
+        document.getElementById("id__" + numero + "-cargo").required = false;
+        document.getElementById("id__" + numero + "-sucursal").required = false;
+        document.getElementById("id__" + numero + "-abono").required = false;
+        document.getElementById(
+            "id__" + numero + "-proveedor",
+        ).required = false;
+        document.getElementById("id__" + numero + "-concepto").required = false;
+        document.getElementById("id__" + numero + "-iva").required = false;
+    }
+};
 
-function visualizacionTab() {
-	if (contador === 0) {
-		colocarValores(1, false);
-		colocarValores(2, false);
-		colocarValores(3, false);
-		colocarValores(4, false);
-		colocarValores(5, false);
-	}
+const visualizacionTab = (num) => {
+    for (let index = 1; index <= num; index++) {
+        if (index === contador) {
+            for (let desdeInicio = 1; desdeInicio <= index; desdeInicio++) {
+                colocarValores(desdeInicio, true);
+            }
+            for (
+                let desdeContador = index + 1;
+                desdeContador <= num;
+                desdeContador++
+            ) {
+                colocarValores(desdeContador, false);
+            }
+        }
+    }
+};
 
-	if (contador === 1) {
-		colocarValores(1, true);
-		colocarValores(2, false);
-		colocarValores(3, false);
-		colocarValores(4, false);
-		colocarValores(5, false);
-	}
-
-	if (contador === 2) {
-		colocarValores(1, true);
-		colocarValores(2, true);
-		colocarValores(3, false);
-		colocarValores(4, false);
-		colocarValores(5, false);
-	}
-	if (contador === 3) {
-		colocarValores(1, true);
-		colocarValores(2, true);
-		colocarValores(3, true);
-		colocarValores(4, false);
-		colocarValores(5, false);
-	}
-	if (contador === 4) {
-		colocarValores(1, true);
-		colocarValores(2, true);
-		colocarValores(3, true);
-		colocarValores(4, true);
-		colocarValores(5, false);
-	}
-	if (contador === 5) {
-		colocarValores(1, true);
-		colocarValores(2, true);
-		colocarValores(3, true);
-		colocarValores(4, true);
-		colocarValores(5, true);
-	}
-}
-
-const tab1 = document.getElementById("pld1");
-const tab2 = document.getElementById("pld2");
-const tab3 = document.getElementById("pld3");
-const tab4 = document.getElementById("pld4");
-const tab5 = document.getElementById("pld5");
 const btnMas = document.getElementById("btnMas");
 const btnMenos = document.getElementById("btnMenos");
-let contador = 0;
+const numDetalles = document.querySelectorAll("[id^=poliza_detalle]").length;
+let contador = 1;
 
-window.onload = visualizacionTab();
+window.onload = visualizacionTab(numDetalles);
 
 btnMas.addEventListener("click", () => {
-	if (contador < 5) {
-		contador++;
-	}
-
-	visualizacionTab();
+    if (contador < numDetalles) {
+        contador++;
+    }
+    visualizacionTab(numDetalles);
 });
 
 btnMenos.addEventListener("click", () => {
-	if (contador > 0) {
-		contador--;
-	}
-	visualizacionTab();
+    if (contador > 1) {
+        contador--;
+    }
+    visualizacionTab(numDetalles);
 });
